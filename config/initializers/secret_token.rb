a813f2864d459979d880d9f753d84a6391413955
LNR <simhaece@gmail.com>
2013-10-29 16:16:01 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pingdoc::Application.config.secret_token = '52d933f5c390bb06b12093ed3384431141f063e5742f039fd011323cdeeddfc44d90ccc5dd55aaa609a52e69ef8cebed6a535e63f719d895cd87449ec767effb'

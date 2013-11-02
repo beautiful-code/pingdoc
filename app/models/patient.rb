b560d297756adebb2dc4d65a436e7c9c8757d53a
Ravi Bhim <ravi.bhim@yahoo.com>
2013-11-02 10:10:03 +0530

    Doc and Patient seeds

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -3,7 +3,7 @@ class Patient
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
@@ -25,17 +25,6 @@ class Patient
   field :last_sign_in_at,    :type => Time
   field :current_sign_in_ip, :type => String
   field :last_sign_in_ip,    :type => String
-
-  ## Confirmable
-  # field :confirmation_token,   :type => String
-  # field :confirmed_at,         :type => Time
-  # field :confirmation_sent_at, :type => Time
-  # field :unconfirmed_email,    :type => String # Only if using reconfirmable
-
-  ## Lockable
-  # field :failed_attempts, :type => Integer, :default => 0 # Only if lock strategy is :failed_attempts
-  # field :unlock_token,    :type => String # Only if unlock strategy is :email or :both
-  # field :locked_at,       :type => Time
 
   #Token authenticatable
   field :authentication_token, :type => String
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Doctor.create(:email => 'doc@foo.com', :password => 'test1234', :password_confirmation => 'test1234') if Doctor.where(:email => 'doc@foo.com').count == 0
+
+Patient.create(:email => 'pat@foo.com', :password => 'test1234', :password_confirmation => 'test1234') if Patient.where(:email => 'pat@foo.com').count == 0

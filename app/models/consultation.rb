e3890f12b3d8d6a1639309f93f51c10946b1bc90
Ravi Bhim <ravi.bhim@yahoo.com>
2013-11-02 11:24:33 +0530

    polymorphic conversations

--- app/models/consultation.rb
+++ app/models/consultation.rb
@@ -7,4 +7,10 @@ class Consultation
 
   belongs_to :doctor
   belongs_to :patient
+
+  has_one :conversation, as: :conversable
+
+  after_save do |cons|
+    Conversation.create!(:conversable_id => cons.id, :conversable_type => cons.class.to_s)
+  end
 end
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -4,4 +4,8 @@ class Conversation
 
   embeds_many :messages
 
+  field :conversable_id, :type => Integer
+  field :conversable_type, :type => String
+
+  belongs_to :conversable, polymorphic: true
 end
